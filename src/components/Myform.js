9850bac65e263078664bd0ad5b4048a096633713
RSainarayan <shivank2002@gmail.com>
2023-12-06 11:47:44 +0530

    formworking

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,65 +1,69 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function MyForm() {
-  const [name, setName] = useState("a");
-  const [email, setEmail] = useState("a");
-  const [number, setNumber] = useState("a");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    alert(`Name: ${name}\nEmail: ${email}\nNumber: ${number}`);
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('number', number);
     
+    const data = {
+      name,
+      email,
+      number
+    };
+
     try {
       const response = await fetch('http://localhost:3001/register', {
         method: 'POST',
-        mode: 'cors',
-        body: formData,
-        
-        
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
       });
-      
-      const data = await response.json(); // Parse response JSON
-      
+
+      const responseData = await response.json();
+
       if (response.ok) {
-        console.log('Data saved successfully:', data);
+        console.log('Data saved successfully:', responseData);
         alert('Data saved successfully!');
       } else {
-        console.error('Failed to save data:', data);
+        console.error('Failed to save data:', responseData);
         alert('Failed to save data.');
       }
     } catch (error) {
       console.error('Error:', error);
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>Enter your name:
+      <label>
+        Enter your name:
         <input 
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
-      <label>Enter your email:
+      <label>
+        Enter your email:
         <input 
           type="text" 
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
       </label>
-      <label>Enter your number:
+      <label>
+        Enter your number:
         <input 
           type="text" 
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
       </label>
-      <input type="submit" />
+      <input type="submit" value="Submit" />
     </form>
   );
 }
